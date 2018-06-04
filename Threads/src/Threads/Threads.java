ad810b1ab929d284450ca9201323f9dc467ed2ec
Kevin <Kevin@DESKTOP-VA267TM>
2018-06-04 18:42:35 +0300

    A Simple implementation of Threads in java

--- Threads/src/Threads/Threads.java
+++ Threads/src/Threads/Threads.java
@@ -17,61 +17,108 @@ import org.jdesktop.xswingx.PromptSupport;
  * @author Kevin
  */
 public class Threads extends javax.swing.JFrame {
+
         AnimationClass ac = new AnimationClass();
-        
-        Thread first=new Thread(){
+        private boolean[] running = {true,true,true,true};
+
+        MyRunnable first = new MyRunnable() {
                 @Override
-                public void run(){
-                        while(true){
-                                try{
-                                        
-                                }catch(Exception e){}
+                public void run() {
+                        isAlive = true;
+                        int count = 0;
+                        lblImages.setIcon(getResizedImage("/Resources/1575882.jpg", lblImages));
+                        while (running[0]) {
+                                try {
+                                        count++;
+                                        if (count == 1) {
+                                                Thread.sleep(2000);
+                                        }
+                                        ac.jLabelXLeft(6, -194, 10, 1, lblImages);
+                                        Thread.sleep(3700);
+
+                                        lblImages.setIcon(getResizedImage("/Resources/image.jpeg", lblImages));
+                                        ac.jLabelXRight(-194, 6, 10, 1, lblImages);
+                                        Thread.sleep(6000);
+                                        ac.jLabelXLeft(6, -194, 10, 1, lblImages);
+                                        Thread.sleep(3700);
+
+                                        lblImages.setIcon(getResizedImage("/Resources/image2.jpg", lblImages));
+                                        ac.jLabelXRight(-194, 6, 10, 1, lblImages);
+                                        Thread.sleep(6000);
+                                        ac.jLabelXLeft(6, -194, 10, 1, lblImages);
+                                        Thread.sleep(3700);
+
+                                        lblImages.setIcon(getResizedImage("/Resources/shutter.jpg", lblImages));
+                                        ac.jLabelXRight(-194, 6, 10, 1, lblImages);
+                                        Thread.sleep(6000);
+                                        ac.jLabelXLeft(6, -194, 10, 1, lblImages);
+                                        Thread.sleep(3700);
+
+                                        lblImages.setIcon(getResizedImage("/Resources/1575882.jpg", lblImages));
+                                        ac.jLabelXRight(-194, 6, 10, 1, lblImages);
+                                        Thread.sleep(6000);
+                                } catch (Exception e) {
+                                }
                         }
+                        lblImages.setIcon(new ImageIcon());
+                        isAlive = false;
+                        setEnabled(btn1, true);
                 }
         };
         
-        Thread second=new Thread(){
+        MyRunnable second = new MyRunnable() {
                 @Override
-                public void run(){
-                        while(true){
-                                try{
-                                        Random temp=new Random();
+                public void run() {
+                        isAlive = true;
+                        while (running[1]) {
+                                try {
+                                        Random temp = new Random();
                                         lblRandom.setText(String.valueOf(temp.nextInt(1000)));
-                                        Thread.sleep(1500);
-                                }catch(Exception e){}
+                                        Thread.sleep(1000);
+                                } catch (Exception e) {
+                                }
                         }
+                        lblRandom.setText("");
+                        isAlive = false;
+                        setEnabled(btn2, true);
+                       
                 }
         };
-        
-        Thread third=new Thread(){
+
+        MyRunnable third = new MyRunnable() {
                 @Override
-                public void run(){
-                        while(true){
-                                try{
+                public void run() {
+                        isAlive = true;
+                        lblSlide.setIcon(getResizedImage("",lblSlide));
+                        while (running[2]) {
+                                try {
                                         
-                                }catch(Exception e){}
+                                } catch (Exception e) {
+                                }
                         }
+                        isAlive = false;
+                        setEnabled(btn3, true);
+       
                 }
         };
-        
-        Thread fourth=new Thread(){
+
+        MyRunnable fourth = new MyRunnable() {
                 @Override
-                public void run(){
-                        while(true){
-                                try{
-                                        Thread.sleep(50);
-                                        String time="";
-                                        time+=Calendar.getInstance().get(Calendar.HOUR_OF_DAY)+" : ";
-                                        time+=Calendar.getInstance().get(Calendar.MINUTE)+" : ";
-                                        time+=Calendar.getInstance().get(Calendar.SECOND)+" : ";
-                                        time+=Calendar.getInstance().get(Calendar.MILLISECOND);
-                                        lblTime.setText(time);
-                                }catch(Exception e){}
+                public void run() {
+                        isAlive = true;
+                        while (running[3]) {
+                                String time = "";
+                                time += Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + " : ";
+                                time += Calendar.getInstance().get(Calendar.MINUTE) + " : ";
+                                time += Calendar.getInstance().get(Calendar.SECOND) + " : ";
+                                time += Calendar.getInstance().get(Calendar.MILLISECOND);
+                                lblTime.setText(time);
                         }
+                        lblTime.setText("");
+                        isAlive = false;
+                        setEnabled(btn4, true);
                 }
         };
-        
-        
 
         /**
          * Creates new form Threads
@@ -79,7 +126,7 @@ public class Threads extends javax.swing.JFrame {
         public Threads() {
                 initComponents();
                 setResizable(false);
-                setSize(660,460);
+                setSize(680, 460);
                 setLocationRelativeTo(null);
                 PromptSupport.setPrompt("Input Here", txtInput);
                 PromptSupport.setForeground(Color.GRAY, txtInput);
@@ -89,8 +136,20 @@ public class Threads extends javax.swing.JFrame {
                 PromptSupport.setForeground(Color.GRAY, txtOutput);
                 PromptSupport.setFontStyle(4, txtOutput);
                 PromptSupport.setFocusBehavior(PromptSupport.FocusBehavior.SHOW_PROMPT, txtOutput);
-                second.start();
-                fourth.start();
+        }
+
+        private ImageIcon getResizedImage(String url, JLabel jlabel) {
+                ImageIcon temp = new ImageIcon(new ImageIcon(getClass().getResource(url)).getImage().getScaledInstance(jlabel.getWidth(), jlabel.getHeight(), Image.SCALE_SMOOTH));
+                return temp;
+        }
+
+        private synchronized void setEnabled(JButton jbutton, boolean state) {
+                jbutton.setEnabled(state);
+                if (btn1.isEnabled() && btn2.isEnabled() && btn3.isEnabled() && btn4.isEnabled()) {
+                        btn5.setEnabled(true);
+                } else {
+                        btn5.setEnabled(false);
+                }
         }
 
         /**
@@ -121,24 +180,44 @@ public class Threads extends javax.swing.JFrame {
                 btn1.setText("Process 1");
                 btn1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
                 btn1.setFocusable(false);
+                btn1.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                btn1ActionPerformed(evt);
+                        }
+                });
                 getContentPane().add(btn1);
                 btn1.setBounds(6, 6, 88, 32);
 
                 btn2.setText("Process 2");
                 btn2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
                 btn2.setFocusable(false);
+                btn2.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                btn2ActionPerformed(evt);
+                        }
+                });
                 getContentPane().add(btn2);
                 btn2.setBounds(112, 6, 88, 32);
 
                 btn3.setText("Process 3");
                 btn3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
                 btn3.setFocusable(false);
+                btn3.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                btn3ActionPerformed(evt);
+                        }
+                });
                 getContentPane().add(btn3);
                 btn3.setBounds(218, 6, 88, 32);
 
                 btn4.setText("Process 4");
                 btn4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
                 btn4.setFocusable(false);
+                btn4.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                btn4ActionPerformed(evt);
+                        }
+                });
                 getContentPane().add(btn4);
                 btn4.setBounds(324, 6, 88, 32);
 
@@ -171,7 +250,7 @@ public class Threads extends javax.swing.JFrame {
                 getContentPane().add(lblRandom);
                 lblRandom.setBounds(6, 246, 194, 172);
                 getContentPane().add(lblSlide);
-                lblSlide.setBounds(218, 56, 194, 172);
+                lblSlide.setBounds(218, -172, 194, 172);
 
                 txtInput.setFont(new java.awt.Font("Courier New", 3, 18)); // NOI18N
                 txtInput.addKeyListener(new java.awt.event.KeyAdapter() {
@@ -189,7 +268,7 @@ public class Threads extends javax.swing.JFrame {
 
                 lblTime.setFont(new java.awt.Font("DigifaceWide", 3, 20)); // NOI18N
                 getContentPane().add(lblTime);
-                lblTime.setBounds(430, 246, 210, 50);
+                lblTime.setBounds(430, 246, 230, 50);
 
                 pack();
         }// </editor-fold>//GEN-END:initComponents
@@ -202,7 +281,22 @@ public class Threads extends javax.swing.JFrame {
         }//GEN-LAST:event_btn5ActionPerformed
 
         private void btn6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn6ActionPerformed
-                // TODO add your handling code here:
+                running[0] = false;
+                running[1] = false;
+                running[2] = false;
+                running[3] = false;
+                if (first.isAlive()) {
+                        btn1.setEnabled(false);
+                }
+                if (second.isAlive()) {
+                        btn2.setEnabled(false);
+                }
+                if (third.isAlive()) {
+                        btn3.setEnabled(false);
+                }
+                if (fourth.isAlive()) {
+                        btn4.setEnabled(false);
+                }
         }//GEN-LAST:event_btn6ActionPerformed
 
         private void txtInputKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtInputKeyReleased
@@ -215,6 +309,38 @@ public class Threads extends javax.swing.JFrame {
                 txtOutput.setCaretPosition(0);
         }//GEN-LAST:event_txtInputKeyReleased
 
+        private void btn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn1ActionPerformed
+                if (!first.isAlive()) {
+                        running[0] = true;
+                        Thread temp = new Thread(first);
+                        temp.start();
+                }
+        }//GEN-LAST:event_btn1ActionPerformed
+
+        private void btn2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn2ActionPerformed
+                if (!second.isAlive()) {
+                        running[1] = true;
+                        Thread temp = new Thread(second);
+                        temp.start();
+                }
+        }//GEN-LAST:event_btn2ActionPerformed
+
+        private void btn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn3ActionPerformed
+                if (!third.isAlive()) {
+                        running[2] = true;
+                        Thread temp = new Thread(third);
+                        temp.start();
+                }
+        }//GEN-LAST:event_btn3ActionPerformed
+
+        private void btn4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn4ActionPerformed
+                if (!fourth.isAlive()) {
+                        running[3] = true;
+                        Thread temp = new Thread(fourth);
+                        temp.start();
+                }
+        }//GEN-LAST:event_btn4ActionPerformed
+
         /**
          * @param args the command line arguments
          */
@@ -226,7 +352,8 @@ public class Threads extends javax.swing.JFrame {
                  */
                 try {
                         UIManager.setLookAndFeel(new DarculaLaf());
-                } catch (UnsupportedLookAndFeelException ex) {}
+                } catch (UnsupportedLookAndFeelException ex) {
+                }
                 //</editor-fold>
 
                 /* Create and display the form */
